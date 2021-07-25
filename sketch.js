fd8645396773fac07666c529b4ac96ad23651793
mlmargaretli6@gmail.com <>
2021-07-24 19:52:55 -0700

    added questions

--- sketch.js
+++ sketch.js
@@ -1,23 +1,27 @@
-//questions: What is your favorite drink? What is your favorite cuisine? What is your favorite dessert? What is your favorite restaurant? What is your favorite sports team/athlete?
-// What kinds of sports/physical activities do you practice?
-// What is your favorite season?
-// What kinds of weather do you like?
-// How is the weather in your area?
-// What was the best vacation you’ve had?
-// What is your favorite nature/amusement park?
-// Which kinds of museums do you like to visit?
-// Which musical instruments do you play?
-// What genres of music do you like?
-// What are your favorite singers/bands?
-// What holidays are celebrated within your culture?
-// What kinds of animals do you own or adore?
-// What is your favorite clothing/jewelry brand?
-// Which kinds of clothes/jewelry do you prefer wearing?
-// What is your favorite car brand?
-// What are your favorite stores of any kind?
-// What is/was your favorite toy?
-// What are your hobbies?
-// What are your favorite colors and why?
+questions = ["What is your favorite drink?";
+" What is your favorite cuisine?";
+"What is your favorite dessert?";
+"What is your favorite restaurant?";
+"What is your favorite sports team/athlete?";
+"What kinds of sports/physical activities do you practie?";
+"What is your favorite season?";
+"What kinds of weather do you like?";
+"How is the weather in your area?";
+"What was the best vacation you’ve had?";
+"What is your favorite nature/amusement park?";
+"Which kinds of museums do you like to visit?";
+"Which musical instruments do you play?";
+"What genres of music do you like?";
+"What are your favorite singers/bands?";
+"What holidays are celebrated within your culture?";
+"What kinds of animals do you own or adore?";
+"What is your favorite clothing/jewelry brand?";
+"Which kinds of clothes/jewelry do you prefer wearing?";
+"What is your favorite car brand?";
+"What are your favorite stores of any kind?";
+"What is/was your favorite toy?";
+"What are your hobbies?";
+"What are your favorite colors and why?"]
 
 
 function setup() {
